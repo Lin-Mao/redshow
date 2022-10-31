2c73f4845ddd85634a11d7b04b333a39ea61d49e
Lin-Mao <linmaosdu@gmail.com>
2022-10-31 14:27:43 -0700

    Update torch monitor

--- include/analysis/torch_monitor.h
+++ include/analysis/torch_monitor.h
@@ -6,17 +6,17 @@
 
 #include "analysis.h"
 #include "binutils/cubin.h"
-#include "common/graph.h"
 #include "common/map.h"
-#include "common/set.h"
-#include "common/path.h"
 #include "common/utils.h"
 #include "operation/kernel.h"
 #include "operation/memcpy.h"
 #include "operation/memory.h"
 #include "operation/memset.h"
+#include "operation/memfree.h"
 #include "operation/operation.h"
 #include "redshow.h"
+
+#include <fstream>
 
 namespace redshow {
 
@@ -51,7 +51,45 @@ class TorchMonitor final : public Analysis {
                      redshow_record_data_callback_func record_data_callback);
 
  private:
-  int a;
+  Map<u64, std::shared_ptr<Memory>> _memories;
+
+  Map<u64, std::shared_ptr<Memory>> _current_memories;
+
+  // <start_addr, memory_op_id>
+  Map<u64, u64> _addresses_map;
+
+  u64 _current_memory_usage = 0;  // to update _memory_peak
+  u64 _memory_peak = 0;
+  u64 _optimal_memory_peak = 0;
+  u64 _memory_peak_kernel = 0;
+
+
+
+
+  Map<u64, std::shared_ptr<Memory>> _submemories;
+
+  Map<u64, std::shared_ptr<Memory>> _current_submemories;
+
+  Map<u64, u64> _sub_addresses_map;
+
+  u64 _current_submemory_usage = 0;  // to update _submemory_peak
+  u64 _submemory_peak = 0;
+  u64 _optimal_submemory_peak = 0;
+  u64 _submemory_peak_kernel = 0;
+
+
+ private:
+  void memory_op_callback(std::shared_ptr<Memory> op, bool is_submemory = false);
+
+  void memfree_op_callback(std::shared_ptr<Memfree> op, bool is_submemory = false);
+
+  void kernel_op_callback(std::shared_ptr<Kernel> op);
+
+  void memcpy_op_callback(std::shared_ptr<Memcpy> op);
+
+  void memset_op_callback(std::shared_ptr<Memset> op);
+
+  void update_aux_hit(void* aux, u64 kernel_op_id, bool is_sub = false);
 
 };  // TorchMonitor
 
--- src/analysis/torch_monitor.cpp
+++ src/analysis/torch_monitor.cpp
@@ -8,8 +8,6 @@
 #include <string>
 
 #include "common/utils.h"
-#include "operation/memcpy.h"
-#include "operation/memset.h"
 
 
 namespace redshow {
@@ -18,18 +16,127 @@ namespace redshow {
 void TorchMonitor::op_callback(OperationPtr op, bool is_submemory /* default = false */) {
   // Add a calling context node
   lock();
+  if (op->type == OPERATION_TYPE_KERNEL) {
+    kernel_op_callback(std::dynamic_pointer_cast<Kernel>(op));
+  } else if (op->type == OPERATION_TYPE_MEMORY) {
+    memory_op_callback(std::dynamic_pointer_cast<Memory>(op), is_submemory);
+  } else if (op->type == OPERATION_TYPE_MEMFREE) {
+    memfree_op_callback(std::dynamic_pointer_cast<Memfree>(op), is_submemory);
+  } else if (op->type == OPERATION_TYPE_MEMCPY) {
+    memcpy_op_callback(std::dynamic_pointer_cast<Memcpy>(op));
+  } else if (op->type == OPERATION_TYPE_MEMSET) {
+    memset_op_callback(std::dynamic_pointer_cast<Memset>(op));
+  }
 
 
   unlock();
 }
 
+void TorchMonitor::memory_op_callback(std::shared_ptr<Memory> op, bool is_submemory) {
+
+  if (!is_submemory) {
+    _memories.try_emplace(op->op_id, op);
+    _current_memories.try_emplace(op->op_id, op);
+    _addresses_map.try_emplace(op->memory_range.start, op->op_id);
+    _current_memory_usage += op->len;
+
+    // printf("op_id:%lu, len:%d\n", op->op_id, op->len);
+    if (_current_memory_usage > _memory_peak)
+      _memory_peak = _current_memory_usage;
+  } else {
+    _submemories.try_emplace(op->op_id, op);
+    _current_submemories.try_emplace(op->op_id, op);
+    _sub_addresses_map.try_emplace(op->memory_range.start, op->op_id);
+    _current_submemory_usage += op->len;
+
+    if (_current_submemory_usage > _submemory_peak) {
+      _submemory_peak = _current_submemory_usage;
+    }
+  }
+}
+
+void TorchMonitor::memfree_op_callback(std::shared_ptr<Memfree> op, bool is_submemory) {
+  // update_global_op_id_start(op->op_id);
+
+  if (!is_submemory) {
+    u64 address = op->memory_range.start;
+    u64 malloc_op_id = _addresses_map.at(address);
+    
+    _addresses_map.erase(address);
+    _current_memories.erase(malloc_op_id);
+    _current_memory_usage -= op->len;
+
+  } else {
+    u64 address = op->memory_range.start;
+    u64 sub_alloc_id = _sub_addresses_map.at(address);
+
+    _sub_addresses_map.erase(address);
+    _current_submemories.erase(sub_alloc_id);
+    _current_submemory_usage -= op->len;
+  }
+}
+
+void TorchMonitor::kernel_op_callback(std::shared_ptr<Kernel> op) {
+
+}
+
+void TorchMonitor::memcpy_op_callback(std::shared_ptr<Memcpy> op) {
+
+}
+
+void TorchMonitor::memset_op_callback(std::shared_ptr<Memset> op) {
+
+}
+
 void TorchMonitor::analysis_begin(u32 cpu_thread, i32 kernel_id, u64 host_op_id, u32 stream_id,
-                            u32 cubin_id, u32 mod_id, GPUPatchType type, void* aux) {
+                            u32 cubin_id, u32 mod_id, GPUPatchType type, void* trace_data) {
   assert(type == GPU_PATCH_TYPE_ADDRESS_PATCH || type == GPU_PATCH_TYPE_ADDRESS_ANALYSIS);
 
   lock();
 
+  gpu_patch_buffer_t* buffer = static_cast<gpu_patch_buffer_t*>(trace_data);
+
+  if (buffer->aux) {
+    update_aux_hit(buffer->aux, host_op_id);
+  }
+
+  if (buffer->torch_aux) {
+    update_aux_hit(buffer->torch_aux, host_op_id, true);
+  }
+
   unlock();
+}
+
+void TorchMonitor::update_aux_hit(void* aux, u64 kernel_op_id, bool is_sub) {
+  gpu_patch_aux_address_dict_t* kernel_aux = static_cast<gpu_patch_aux_address_dict_t*>(aux);
+
+  if (!is_sub) {
+    u64 kernel_memory_usage = 0;
+    for (int i = 0; i < kernel_aux->size; i++) {
+      if (kernel_aux->hit[i] == 1) {
+        u64 memory_op_id = _addresses_map.at(kernel_aux->start_end[i].start);
+        kernel_memory_usage += _current_memories.at(memory_op_id)->len;
+      }
+    }
+
+    if (_optimal_memory_peak < kernel_memory_usage) {
+      _memory_peak_kernel = kernel_op_id;
+      _optimal_memory_peak = kernel_memory_usage;
+    }
+  } else {
+    u64 kernel_submemory_usage = 0;
+    for (int i = 0; i < kernel_aux->size; i++) {
+      if (kernel_aux->hit[i] == 1) {
+        u64 sub_memory_op_id = _sub_addresses_map.at(kernel_aux->start_end[i].start);
+        kernel_submemory_usage += _current_submemories.at(sub_memory_op_id)->len;
+      }
+    }
+
+    if (_optimal_submemory_peak < kernel_submemory_usage) {
+      _submemory_peak_kernel = kernel_op_id;
+      _optimal_submemory_peak = kernel_submemory_usage;
+    }
+  }
 }
 
 void TorchMonitor::analysis_end(u32 cpu_thread, i32 kernel_id) {}
@@ -53,8 +160,17 @@ void TorchMonitor::flush_thread(u32 cpu_thread, const std::string &output_dir,
 
 void TorchMonitor::flush(const std::string &output_dir, const LockableMap<u32, Cubin> &cubins,
                      redshow_record_data_callback_func record_data_callback) {
+  std::ofstream output(output_dir + "memory_info.txt");
+  output << "GPU memory peak: " << _memory_peak << " B" << std::endl;
+  output << "Optimal GPU memory peak: " << _optimal_memory_peak << " B" << std::endl;
+  output << "Peak kernel op id: " << _memory_peak_kernel << std::endl;
+  output << std::endl;
 
+  output << "Submemory peak: " << _submemory_peak << " B" << std::endl;
+  output << "Optimal submemory peak: " << _optimal_submemory_peak << " B" << std::endl;
+  output << "Peak kernel op id: " << _submemory_peak_kernel << std::endl;
 
+  output.close();
 }
 
 }  // namespace redshow
